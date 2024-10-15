fac8dbb32373918c37e9a8a4cf9e79350cd04b61
Gabriell1507 <gabrielmarcosbguimaraes@gmail.com>
2024-10-15 20:41:47 -0300

    subindo configurações

---
+++ postgres/scripts/prepara-postgres.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+function cria_usuario_e_bd() {
+    echo "Criando usuário DSpace"
+    psql --username=postgres -c "CREATE USER dspace WITH PASSWORD 'U9m0QKa25P82evCN';"
+    psql --username=postgres -c "CREATE DATABASE dspace WITH OWNER dspace;"
+    psql --username=postgres -c "GRANT ALL PRIVILEGES ON DATABASE dspace TO dspace;"
+}
+
+function instala_pg_crypto() {
+    echo "Criando pgCrypto"
+    psql --username=postgres dspace -c "CREATE EXTENSION pgcrypto;"
+}
+
+cria_usuario_e_bd
+instala_pg_crypto
+
+if [[ -f /opt/dump.sql ]]; then
+  echo "Identificado arquivo de DUMP efetuando a restauração"
+  psql -d dspace -U dspace -f /opt/dump.sql
+fi
---
+++ restart-backend.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker rm -f dspace7 || true > /dev/null 2>&1
+docker rm -f dspace7db || true > /dev/null 2>&1
+docker rm -f dspace7solr || true > /dev/null 2>&1
+
+
+docker rmi -f ibict/postgresdspace7 || true > /dev/null 2>&1
+docker rmi -f solr:8.11-slim || true > /dev/null 2>&1
+
+
+
+docker compose -f docker-compose_restart.yml up --build
