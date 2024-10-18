e1c1fa9ac1f0ca35b31e73cbd8be780f833300be
Francisco <francisco.carvalho.q@gmail.com>
2024-10-18 14:25:44 -0300

    script de colecoes: adiciona metadado de regiao nos itens

---
+++ script-colecoes.sql
@@ -0,0 +1,36 @@
+```sql
+-- Adiciona metadata dc.description.region no registro de metadados
+INSERT INTO metadatafieldregistry (metadata_schema_id, element, qualifier) VALUES ((SELECT metadata_schema_id FROM metadataschemaregistry WHERE short_id = 'dc'), 'description', 'region');
+
+-- Adiciona o metadado de região para cada item
+INSERT INTO metadatavalue (metadata_field_id, text_value, text_lang, place, authority, dspace_object_id)
+SELECT
+  (
+    SELECT metadata_field_id
+    FROM metadatafieldregistry
+    WHERE
+      metadata_schema_id = (SELECT metadata_schema_id FROM metadataschemaregistry where short_id = 'dc') AND
+      element = 'description' AND
+      qualifier = 'region'
+  ) AS metadata_field_id,
+  (
+    SELECT text_value FROM metadatavalue
+    WHERE dspace_object_id = collection.uuid AND
+          metadata_field_id = (
+                                SELECT metadata_field_id
+                                FROM metadatafieldregistry
+                                WHERE
+                                  metadata_schema_id = (SELECT metadata_schema_id FROM metadataschemaregistry where short_id = 'dc') AND
+                                  element = 'title' AND
+                                  qualifier IS NULL
+                              )
+  ) AS text_value,
+  'por' AS text_lang,
+  0 AS place,
+  '' AS authority,
+  item.uuid AS dspace_object_id
+FROM
+item
+JOIN collection ON item.owning_collection = collection.uuid
+WHERE item.in_archive = 't';
+```
