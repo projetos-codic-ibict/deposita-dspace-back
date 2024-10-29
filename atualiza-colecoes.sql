4b6228a8a1657caba5de03a3e99b7e6de1c0f58d
Francisco <francisco.carvalho.q@gmail.com>
2024-10-29 11:50:55 -0300

    mais consertos

--- atualiza-colecoes.sql
+++ atualiza-colecoes.sql
@@ -9,21 +9,21 @@ WITH novas_colecoes AS (
     WHERE owning_collection IS NULL OR owning_collection NOT IN (SELECT uuid FROM novas_colecoes)
 ), itens_mais_tipos AS (
     SELECT item.*, mv.text_value tipo
-    FROM itens_a_terem_colecoes_atualizadas item
+    FROM itens_a_terem_colecoes_atualizadas AS item
     JOIN metadatavalue mv
     ON
         mv.metadata_field_id = get_metadata_field_id('type', NULL) AND
         mv.dspace_object_id = item.uuid
 ), colecoes_mais_titulos AS (
     SELECT colecao.*, mv.text_value titulo
-    FROM collection colecao
+    FROM collection AS colecao
     JOIN metadatavalue mv
     ON
         mv.metadata_field_id = get_metadata_field_id('title', NULL) AND
         mv.dspace_object_id = colecao.uuid
 ), itens_mais_novos_uuids AS (
     SELECT item.*, colecao.uuid AS uuid_da_nova_colecao
-    FROM itens_mais_tipos item
+    FROM itens_mais_tipos AS item
     JOIN colecoes_mais_titulos colecao
     ON
         item.tipo = colecao.titulo
@@ -31,14 +31,14 @@ WITH novas_colecoes AS (
     UPDATE item
     SET owning_collection = (
         SELECT uuid_da_nova_colecao
-        FROM itens_mais_novos_uuids item2
+        FROM itens_mais_novos_uuids AS item2
         WHERE item.uuid = item2.uuid
     )
 ), atualiza_em_cwf_workflowitem AS (
     UPDATE cwf_workflowitem
     SET collection_id = (
         SELECT uuid_da_nova_colecao
-        FROM itens_mais_novos_uuids item2
+        FROM itens_mais_novos_uuids AS item2
         WHERE cwf_workflowitem.item_id = item2.uuid
     )
 )
@@ -46,7 +46,7 @@ WITH novas_colecoes AS (
 UPDATE workspaceitem
 SET collection_id = (
     SELECT uuid_da_nova_colecao
-    FROM itens_mais_novos_uuids item2
+    FROM itens_mais_novos_uuids AS item2
     WHERE workspaceitem.item_id = item2.uuid
 )
 
@@ -58,5 +58,31 @@ SELECT
     item.uuid AS item_id
 FROM item
 WHERE item.owning_collection IS NOT NULL
+
+;
+
+UPDATE cwf_pooltask AS pt
+SET group_id = (
+    SELECT uuid
+    FROM epersongroup
+    WHERE name = CONCAT(
+        'COLLECTION_',
+        (
+            SELECT collection_id
+            FROM collection
+            WHERE uuid = (
+                SELECT collection_id
+                FROM cwf_workflowitem AS wi
+                WHERE wi.workflowitem_id = pt.workflowitem_id
+            )
+        ),
+        '_WORKFLOW_STEP_2'
+    )
+)
+WHERE group_id IN (
+    SELECT uuid
+    FROM epersongroup
+    WHERE name LIKE 'COLLECTION\_%\_WORKFLOW\_STEP\_2' ESCAPE '\'
+)
 
 ;
