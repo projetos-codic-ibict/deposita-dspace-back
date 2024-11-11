16de3994a1a7d2a625fae7a7cd4901d7b58631d0
Francisco <francisco.carvalho.q@gmail.com>
2024-11-11 14:38:36 -0300

    coisas de antes

--- dspace/src/main/docker/dspace-postgres-deposita/import-dump.sh
+++ dspace/src/main/docker/dspace-postgres-deposita/import-dump.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -f "/dump/dump.sql"
+# psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -f "/dump/dump.sql"
